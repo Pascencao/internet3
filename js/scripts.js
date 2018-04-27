10f1684478a691fd72c1682e15484858ff683ba5
pascencao <pandresascencao@gmail.com>
2018-04-27 19:17:26 -0300

    functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,26 @@
 var users = [
-    {
-        name: 'Pablo',
-        lastname: 'ascencao',
-        age: 31,
-        children: ['Pedro','Juan'],
-        email: 'pablo.ascencao@gmail.com'
-    },
+    { name: 'Pablo',lastname: 'ascencao',age: 31,children: ['Pedro','Juan'],email: 'pablo.ascencao@gmail.com'},
     { name: 'alejandro1',lastname: 'rausso',age: 25,children: ['hola','como', 'te', 'va'],email: 'ale.russo@otmail.com'},
     { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
     { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-    {
-        name: 'Pablo',
-        lastname: 'ascencao',
-        age: 31,
-        children: ['Pedro','Juan'],
-        email: 'pablo.ascencao@gmail.com'
-    },
+    { name: 'Pablo',lastname: 'ascencao',age: 31,children: ['Pedro','Juan'],email: 'pablo.ascencao@gmail.com'},
     { name: 'alejandro1',lastname: 'rausso',age: 25,children: ['hola','como', 'te', 'va'],email: 'ale.russo@otmail.com'},
     { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
     { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-    {
-        name: 'Pablo',
-        lastname: 'ascencao',
-        age: 31,
-        children: ['Pedro','Juan'],
-        email: 'pablo.ascencao@gmail.com'
-    },
-    { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-    { name: 'alejandro3',lastname: 'rusdso',age: 25,children: [],email: 'ale.russo@otmail.com' }
 ]
 
-for(var i=0; i < users.length; i++){
-    console.log(users[i].lastname + ', ' + users[i].name + ' tiene ' + users[i].children.length + ' hijos ');
+function filterByAge(age){
+    for(var i=0; i < users.length; i++){
+        if ( age => users[i].age ){
+            console.log( assamble( users[i] ) );
+        }
+    }
+}
+
+function assamble(user){
+    return user.lastname 
+            + ', ' 
+            + user.name 
+            + ' tiene ' 
+            + user.children.length + ' hijos a la edad de '+ user.age;
 }
