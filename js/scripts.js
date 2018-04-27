1c672ae68dbbbe3b374a6f304eb6dadc74c64157
pascencao <pandresascencao@gmail.com>
2018-04-27 19:34:32 -0300

    'objects'

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
-var users = [
-    { name: 'Pablo',lastname: 'ascencao',age: 31,children: ['Pedro','Juan'],email: 'pablo.ascencao@gmail.com'},
-    { name: 'alejandro1',lastname: 'rausso',age: 25,children: ['hola','como', 'te', 'va'],email: 'ale.russo@otmail.com'},
-    { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-    { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-    { name: 'Pablo',lastname: 'ascencao',age: 31,children: ['Pedro','Juan'],email: 'pablo.ascencao@gmail.com'},
-    { name: 'alejandro1',lastname: 'rausso',age: 25,children: ['hola','como', 'te', 'va'],email: 'ale.russo@otmail.com'},
-    { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-    { name: 'alejandro2',lastname: 'russso',age: 25,children: ['jorge','augusto','romina'],email: 'ale.russo@otmail.com'},
-]
 
-function filterByAge(age){
+/* function filterByAge(age){
     for(var i=0; i < users.length; i++){
         if ( age => users[i].age ){
             console.log( assamble( users[i] ) );
         }
     }
-}
+} */
 
-function assamble(user){
+/* function assamble(user){
     return user.lastname 
             + ', ' 
             + user.name 
             + ' tiene ' 
             + user.children.length + ' hijos a la edad de '+ user.age;
+} */
+
+var listUsers = {
+    assamble: function (user) {
+        return user.lastname
+            + ', '
+            + user.name
+            + ' tiene '
+            + user.children + ' hijos a la edad de ' + user.age;
+    },
+    iterate: function (usersList) {
+        for (var i = 0; i < usersList.length; i++) {
+            console.log(listUsers.assamble(usersList[i]));
+        }
+    }
 }
+listUsers.assamble(users)
+
+
+
---
+++ js/users.js
@@ -0,0 +1,10 @@
+var users = [
+    { name: 'Pablo', lastname: 'ascencao', age: 31, children: ['Pedro', 'Juan'], email: 'pablo.ascencao@gmail.com' },
+    { name: 'alejandro1', lastname: 'rausso', age: 25, children: ['hola', 'como', 'te', 'va'], email: 'ale.russo@otmail.com' },
+    { name: 'alejandro2', lastname: 'russso', age: 25, children: ['jorge', 'augusto', 'romina'], email: 'ale.russo@otmail.com' },
+    { name: 'alejandro2', lastname: 'russso', age: 25, children: ['jorge', 'augusto', 'romina'], email: 'ale.russo@otmail.com' },
+    { name: 'Pablo', lastname: 'ascencao', age: 31, children: ['Pedro', 'Juan'], email: 'pablo.ascencao@gmail.com' },
+    { name: 'alejandro1', lastname: 'rausso', age: 25, children: ['hola', 'como', 'te', 'va'], email: 'ale.russo@otmail.com' },
+    { name: 'alejandro2', lastname: 'russso', age: 25, children: ['jorge', 'augusto', 'romina'], email: 'ale.russo@otmail.com' },
+    { name: 'alejandro2', lastname: 'russso', age: 25, children: ['jorge', 'augusto', 'romina'], email: 'ale.russo@otmail.com' },
+]
